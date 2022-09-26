1ab8b1f3035606b8d163b300809b80f6c5ac17c1
Devintel <devintelartbay@gmail.com>
2022-09-26 19:42:59 +0100

    unknown

--- src/App.js
+++ src/App.js
@@ -1,90 +1,120 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ProductAlert from './image/product-alert.png'
+import ProductAlert from './image/product-tag.svg';
 import TimeIcon from './image/time-icon.png';
-import AppStore from './image/app-store.png';
-import SnapShot1 from './image/feature1.png';
-import SnapShot2 from './image/feature2.png';
+import AppStore from './image/appstore.svg';
+import SnapShot1 from './image/macstories.svg';
+import SnapShot2 from './image/9to5.svg';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import Footer from './Footer/footer';
+import TweetBird from './image/tweet-bird.jpg';
+import friendSnippet from './image/friendly-logo.svg'
+import PhoneView from './image/phone-image.png';
 import './App.css';
-import RightBarContainer from './RightBar-Container/rightBar-container';
 
 function App() {
   return (
-    <div className='main-container'>
-      <img className='product-tag' 
-        alt='product-alert' src={ProductAlert}
-        />
-      <div className='col-wrapper'>
-      <div className='left-bar'>
+    <div className='body-wrapper'>
+      <div>
+          <img className='product-tag'
+            alt='product-alert' src={ProductAlert}
+          />
+          </div>
+      <div className='col-main'>
+        <div className='col-left'>
         <img className='time-icon' 
         alt='time' src={TimeIcon} 
-        height='auto' width='60px'
+        height='60px' width='60px'
         />
         <p className='time-tag'>Time Zone Pro</p>
         <button className='btn'>For iPhone, iPad and Mac</button>
 
-        <div className='text-menu'>
-          <h1>What time is it there?
-          </h1>
-          <p className='followUp'>
+        <h1 className='header-text'>
+          What time is it there?
+        </h1>
+        <div className='followUp'>
             Never ask that again
-          </p>
-          <p className='nextUp'>
-            Quickly see the local time of your team, friends, and family.
-          </p>
-          <img className='app-store' 
-          alt='app-store' src={AppStore} 
-          height='auto' width='180px'
-          />
-         <nav className='link-list'>
-         <ul>
-            <li>
-              <FontAwesomeIcon icon={faCheck} color='#fff' />
-              iCloud Sync
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faCheck} color='#fff' />
-              Time Scrubbing
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faCheck} color='#fff' />
-              Widgets
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faCheck} color='#fff' />
-              Dark mode
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faCheck} color='#fff' />
-              Night indicator
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faCheck} color='#fff' />
-              Mac and iPhone
-            </li>
-          </ul>
-         </nav>
         </div>
-         <RightBarContainer />
-          <div className='feature'>
-            <p>Featured on</p>
-            <img 
-            alt='feature-shot' src={SnapShot1} 
-            height='auto' width='130px'
-            />
-            <img 
-            alt='feature-shot' src={SnapShot2} 
-            height='50px' width='130px'
+        <div className='nextUp'>
+            Quickly see the local time of your team, friends, and family.
+        </div>
+          <a href='#main' className='app-link'  target='_blank'>
+            <img className='app-store' 
+            alt='app-store' src={AppStore} 
+            height='67px' width='200px'
+          />
+          </a>
+
+          <div className='col-links'>
+          <div>
+          <FontAwesomeIcon icon={faCheck} color='#fff' style={{height: '8px', width: '10px', padding: '4px' }} />
+            iCloud Sync
+          </div>
+          <div>
+          <FontAwesomeIcon icon={faCheck} color='#fff' style={{height: '8px', width: '10px', padding: '4px' }} />
+            Time Scrubbing
+          </div>
+          <div>
+          <FontAwesomeIcon icon={faCheck} color='#fff' style={{height: '8px', width: '10px', padding: '4px' }} />
+            Widgets
+          </div>
+          <div>
+          <FontAwesomeIcon icon={faCheck} color='#fff' style={{height: '8px', width: '10px', padding: '4px' }} />
+            Dark mode
+          </div>
+          <div>
+          <FontAwesomeIcon icon={faCheck} color='#fff' style={{height: '8px', width: '10px', padding: '4px' }} />
+            Night indicator
+          </div>
+          <div>
+          <FontAwesomeIcon icon={faCheck} color='#fff' style={{height: '8px', width: '10px', padding: '4px' }} />
+            Mac and iPhone
+          </div>
+          </div>
+
+          <div className='col-right'>
+          <img className="rightView" 
+            alt="phone" src={PhoneView}
             />
           </div>
-        <br />
-        <br />
-        <Footer />
-      </div>
-      </div>
+          
+        </div>
+
+          <div className='col-end'>
+
+          <div className='feature'>
+            Featured on
+         
+            <div className='boulders'>
+              <a href='#main' className='boulder-1' target='_blank'>
+                <img 
+                alt='feature-shot' src={SnapShot1} 
+                width='120px'/> </a>
+              <a href='#main' className='boulder-2' target='_blank'>
+              <img 
+              alt='feature-shot' src={SnapShot2} 
+              width='120px'/> </a>
+            </div>
+          </div>
+            <a href='#main' className='tweetbird' target='_blank'>
+               <img className='birdie'
+               alt="tweet" src={TweetBird} 
+               width='25px' />
+            </a>
+            <div className='tribute'>Designed and built by</div>
+             <a href='#main' className='friend' target='_blank'>
+              <img alt='friend' src={friendSnippet} 
+              width='70px' height='21px' />
+             </a>
+
+             <div className='support-tag'>
+              <a href='#main'>
+              App Support
+              </a>
+             </div>
+
+          </div>
     </div>
+  </div>
   )
 }
 export default App;
